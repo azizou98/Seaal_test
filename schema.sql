d4ea3be5a56ba6964d207f738a53a50b704b9944
ABDELAZIZ AITMOUSSA <57140192+azizou98@users.noreply.github.com>
2025-07-07 14:19:08 +0000

    comfirm the add

--- schema.sql
+++ schema.sql
@@ -19,14 +19,7 @@ CREATE TABLE Articles (
     stock_status ENUM('in', 'out') DEFAULT 'in',
     INDEX idx_name (name)
 );
-CREATE TABLE Article_In (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    article_id INT NOT NULL,
-    user_id INT NOT NULL,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (article_id) REFERENCES Articles(id),
-    FOREIGN KEY (user_id) REFERENCES Users(id)
-);
+
 CREATE TABLE Article_Out (
     id INT AUTO_INCREMENT PRIMARY KEY,
     article_id INT NOT NULL,
